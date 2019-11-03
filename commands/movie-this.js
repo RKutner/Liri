57bb131344b004fd413acb51e5f0d90cd0d8db9a
Rob <Rob.Kutner@Gmail.com>
2019-11-03 10:38:41 -0800

    omdb is working

--- commands/movie-this.js
+++ commands/movie-this.js
@@ -2,8 +2,4 @@ import axios from "axios";
 
 
 
-axios({
-    method: 'get',
-    title= process.argv[3],
-    url: `http://www.omdbapi.com/?t=${title}&y=&plot=short&apikey=trilogy`
-})
+    export default Movies;
--- liri.js
+++ liri.js
@@ -1,25 +1,39 @@
 require("dotenv").config();
+const axios = require('axios')
 var keys = require("./keys.js");
-const concert= require("./commands/concert-this.js")
-const doWhatItSays= require("./commands/do-what-it-says.js")
-const movieThis= require("./commands/movie-this.js")
-const spotify= require("./commands/spotify.js")
+// const concert= require("./commands/concert-this.js")
+// const doWhatItSays= require("./commands/do-what-it-says.js")
+// const movieThis= require("./commands/movie-this.js")
+// const spotify= require("./commands/spotify.js")
 
-// liri commands
-// 'concert-this'
-// 'spotify'
-// 'movie-this'
-// 'do-what-it-says'
 
-switch(process.argv[2]){
+switch (process.argv[2]) {
     case 'concert-this':
-        console.log("concert");
+        const band = process.argv[3];
+        axios.get(`"https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp"`).then(
+            function (response) {
+                console.log(response);
+            }
+        )
         break;
     case 'spotify':
         console.log("spotify");
         break;
     case 'movie-this':
-        console.log('movie-this');
+        const title = process.argv[3];
+        axios.get(`http://www.omdbapi.com/?t=${title}&y=&plot=short&apikey=trilogy`).then(
+            function (response) {
+                console.log('Title:', response.data.Title);
+                console.log('Year:', response.data.Year);
+                console.log('IMDB Rating:', response.data.imdbRating);
+                console.log('Rotten Tomatoes:', response.data.Ratings[1].Value);
+                console.log('Country:', response.data.Country);
+                console.log('Language:', response.data.Language);
+                console.log('Plot:', response.data.Plot);
+                console.log('Cast:', response.data.Actors);
+
+
+            });
         break;
     case 'do-what-it-says':
         console.log('dowhatitsays');
